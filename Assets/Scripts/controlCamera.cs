578549896a094fb3132149520ee815d45d2bad65
MiguelAn05 <miguelingil89@gmail.com>
2024-02-07 17:52:09 -0500

    seguimiento camara

---
+++ Assets/Scripts/controlCamera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlCamera : MonoBehaviour
+{
+    public GameObject objeto;
+    private Vector3 posicion;
+    public float adelante;
+    public float Smoothing;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posicion = new Vector3(objeto.transform.position.x,objeto.transform.position.y,transform.position.z);
+
+        if (objeto.transform.localScale.x == 1)
+        {
+            posicion = new Vector3(posicion.x + adelante,posicion.y,transform.position.z);
+        }
+
+        if (objeto.transform.localScale.x == -1)
+        {
+            posicion = new Vector3(posicion.x - adelante, posicion.y, transform.position.z);
+            
+        }
+
+        transform.position = Vector3.Lerp(transform.position, posicion, Smoothing * Time.deltaTime);
+
+    }
+
+
+
+}
